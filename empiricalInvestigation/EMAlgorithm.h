b346d8977297aadd5ac857665a375ad597e185ce
Dorian Rojas-Villalta <rojasvillaltadorian@gmail.com>
2026-02-11 14:13:10 -0700

    CV trial

--- empiricalInvestigation/EMAlgorithm.h
+++ empiricalInvestigation/EMAlgorithm.h
@@ -100,7 +100,7 @@ struct EMAlgorithm {
     double denom;
     const double alpha_limit = 1e-7;
     const double alpha_change_limit = 1e-2;
-    const double alpha_change = 1e-2;
+    const double alpha_change = 2.0;
     bool finalRound = false;
 
     if (verbose) {
